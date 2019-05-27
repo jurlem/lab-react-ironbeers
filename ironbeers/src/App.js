e7290807753c2d07d0e3726c4ec7e0193bb6964b
Merle Jansen <merlevanjansen@gmail.com>
2019-05-27 23:20:24 +0200

    testing git

--- ironbeers/src/App.js
+++ ironbeers/src/App.js
@@ -1,28 +1,28 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Home from './Components/Home';
-import { Switch, Route } from 'react-router-dom';
+import {Switch, Route} from 'react-router-dom';
 import Beers from './Components/Beers';
 import NewBeer from './Components/NewBeer';
 import RandomBeer from './Components/RandomBeer';
 import BeerDetail from './Components/BeerDetail';
 
 class App extends Component {
-	render() {
-		return (
-			<main className="container">
-				<div className="App">
-					<Switch>
-						<Route exact path="/" component={Home} />
-						<Route path="/beers/:beerId" component={BeerDetail} />
-						<Route path="/beers" component={Beers} />
-						<Route path="/new-beer" component={NewBeer} />
-						<Route path="/random-beer" component={RandomBeer} />
-					</Switch>
-				</div>
-			</main>
-		);
-	}
+  render () {
+    return (
+      <React.Element className="container">
+        <div className="App">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/beers/:beerId" component={BeerDetail} />
+            <Route path="/beers" component={Beers} />
+            <Route path="/new-beer" component={NewBeer} />
+            <Route path="/random-beer" component={RandomBeer} />
+          </Switch>
+        </div>
+      </React.Element>
+    );
+  }
 }
 
 export default App;
